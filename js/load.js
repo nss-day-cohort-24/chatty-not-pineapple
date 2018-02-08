18eb014d7e34b0e33a0eecbf618869230a510e5b
Ryan Brisentine <brisentine.ryan@gmail.com>
2018-02-08 11:46:32 -0600

    out of lib

--- js/load.js
+++ js/load.js
@@ -4,24 +4,24 @@ var convoRequest = new XMLHttpRequest();
 convoRequest.addEventListener("load", convoRequestComplete);
 
 function convoRequestComplete(event) {
-    var retrievedConvo = JSON.parse(event.target); //error
-    showData(retrievedConvo);
+	var retrievedConvo = JSON.parse(event.target.responseText); //error
+	showData(retrievedConvo);
 }
 
 
-function showData(itDontMatter){
-	var five = document.getElementById("five-conversation");  //need to put id in html
+function showData(dataArray){
+	var preHistory = document.getElementById("message-history-div");  //need to put id in html
 
-  for(whatever in itDontMatter) {                  //added to DOM
-    var convoFiveData = '';
-    var convoItem = itDontMatter[whatever];
-    convoData += "<div>";
-    convoData += "<h4>" + convoItem.name + "</h4>"
-    convoData += "<h2>" + convoItem.conversation + "</h2>"
-    convoData += "</div>";
+	for(var entry in dataArray) {                  // Print to "message-history-div"
+		var convoData = "";
+		var convoItem = dataArray[entry];
+		convoData += "<div>";
+		convoData += "<h4>" + convoItem.username + "</h4>";
+		convoData += "<h2>" + convoItem.conversation + "</h2>";
+		convoData += "</div>";
 
-    five.innerHTML += convoFiveData;
-  }
+		preHistory.innerHTML += convoData;
+	}
 
 }
 
