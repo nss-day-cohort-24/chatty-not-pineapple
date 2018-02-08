b37cb673379158ed25119f79efb7d0f257826b7d
Kelsey Pintens <ksepintens@gmail.com>
2018-02-08 11:57:54 -0600

    added delete js

---
+++ js/delete.js
@@ -0,0 +1,15 @@
+var deleteAll = document.getElementById("deleteAllButton");
+
+function deleteInput () {
+    document.getElementById("message-history-div").innerHTML = " ";
+  }
+
+  deleteAll.addEventListener("click", deleteInput);
+
+  var deleteConversation = document.getElementById(" ");
+
+  function deleteConvo () {
+      document.getElementById(" ").innerHTML = " ";
+    }
+  
+    deleteConversation.addEventListener("click", deleteConvo);
