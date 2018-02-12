f5f98a94a9f371872e24e8daa9035d6e6da09e37
Kelsey Pintens <ksepintens@gmail.com>
2018-02-11 20:41:39 -0600

    checkbox

--- js/checkboxes.js
+++ js/checkboxes.js
@@ -9,8 +9,12 @@ var message = document.getElementsByClassName("username-item");
 
 function darkSide() {
     if (darkBackground.checked === true) {
-        message.style.color = "blue";
         console.log("the box is checked");
+        var elements = document.getElementsByClassName('ace_features'), i, len;
+  
+        for (i = 0, message.length; i < len; i++) {
+          message[i].style.color = '#2C81B7';
+        }
     }
     
 }
@@ -18,7 +22,7 @@ darkBackground.addEventListener("click", darkSide);
 
 var changeText = document.getElementById("text-box");
 // var checkBoxLarge = document.getElementById("inlineCheckbox2");
-var message = document.getElementsByClassName("navbar");
+var message = document.getElementsByClas("navbar");
 // var messagesText = document.getElementsByClassName("message-post");
 
 function largeText() {
