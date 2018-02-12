b5823706921d51cde1f0939e8c22cc02374bdebb
Kelsey Pintens <ksepintens@gmail.com>
2018-02-11 21:23:34 -0600

    change

--- js/checkboxes.js
+++ js/checkboxes.js
@@ -7,33 +7,26 @@ var darkBackground = document.getElementById("dark-box");
 
 function darkSide() {
     var message = document.getElementsByClassName("username-item"), i, len;
+    var background = document.getElementsByClassName("conversation-item"), x, taco;
     if (darkBackground.checked === true) {
         console.log("the box is checked");
         for (i = 0, len = message.length; i < len; i++) {
           message[i].style.color = '#2C81B7';
         }
+        for (x = 0, taco = background.length; x < taco; x++) {
+            background[x].style.backgroundColor = '#aaaaaa';
+          }
     }else if(darkBackground.checked === false) {
         for (i = 0, len = message.length; i < len; i++) {
           message[i].style.color = '#ffffff';
         }
+        for (x = 0, taco = background.length; x < taco; x++) {
+            background[x].style.backgroundColor = '#e7e7e7';
+           }
     }
 }
 
-function darkground() {
-    var background = document.getElementsByClassName("conversation-item"), i, len;
-    if (darkBackground.checked === true) {
-        console.log("the box is checked");
-        for (i = 0, len = background.length; i < len; i++) {
-          background[i].style.backgroundColor = '#aaaaaa';
-        }
-    }else if(darkBackground.checked === false) {
-        for (i = 0, len = background.length; i < len; i++) {
-         background[i].style.backgroundColor = '#e7e7e7';
-        }
-    }
-    
-}
-darkBackground.addEventListener("click", darkSide, darkground);
+darkBackground.addEventListener("click", darkSide);
 
 var changeText = document.getElementById("text-box");
 // var checkBoxLarge = document.getElementById("inlineCheckbox2");
@@ -56,5 +49,5 @@ function darkSide() {
 }
 darkBackground.addEventListener("click", darkSide);
 
-module.exports = {darkSide, darkground};
+module.exports = {darkSide};
 
