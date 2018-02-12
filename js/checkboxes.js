e44fa365f8afe1dbe8121be839c2c36b3dafb083
Kelsey Pintens <ksepintens@gmail.com>
2018-02-11 20:47:10 -0600

    changes

--- js/checkboxes.js
+++ js/checkboxes.js
@@ -22,7 +22,7 @@ darkBackground.addEventListener("click", darkSide);
 
 var changeText = document.getElementById("text-box");
 // var checkBoxLarge = document.getElementById("inlineCheckbox2");
-var message = document.getElementsByClass("navbar");
+var message = document.getElementsByClassName("navbar");
 // var messagesText = document.getElementsByClassName("message-post");
 
 function largeText() {
