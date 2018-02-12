e9ab3d48b84a5a806ee5aeb2b3cc7fb27f0ed026
Kelsey Pintens <ksepintens@gmail.com>
2018-02-12 09:02:04 -0600

    changes

--- js/main.js
+++ js/main.js
@@ -2,11 +2,8 @@
 
 let loader = require("./load");
 let publisher = require("./publish");
-<<<<<<< HEAD
 let style = require("./checkboxes");
-=======
 let remover = require("./delete");
->>>>>>> master
 
 
 console.log("test");
