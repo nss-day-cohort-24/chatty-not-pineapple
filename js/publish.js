916cd6629cf64814b4f98d648144d8ade74327d1
Kelsey Pintens <ksepintens@gmail.com>
2018-02-12 09:47:04 -0600

    change

--- js/publish.js
+++ js/publish.js
@@ -29,7 +29,7 @@ document.getElementById("send-button").addEventListener("click", function( event
 	var tempString = "<div class=message-entry>" +
 	"<div class=username-item> " + "Jisie" + "</div>" +
 	"<div class=conversation-item>" + temp + "</div>" +
-	"<button id=delete-button img=images/trash.svg></button></div> ";
+	"<button id=delete-button>DELETE</button></div> ";
 	appendDOM.innerHTML += tempString;
 
 	document.getElementById("enter-text-field").value ="";
